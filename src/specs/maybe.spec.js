0dba8a36b2464b177b1bfdd77d6161644b0dab15
Pedro de Jesus <pedro.jesus@talkdesk.com>
2019-04-26 13:51:11 +0100

    added tests for the Maybe monad

--- src/specs/maybe.spec.js
+++ src/specs/maybe.spec.js
@@ -1,36 +1,72 @@
 import Maybe from '../monads/maybe'
 
 describe('Maybe', () => {
+  describe('.flatten', () => {
+    it('returns the value in the monad', () => {
+      const value = 5
+      const maybeValue = Maybe.of(5)
+
+      expect(maybeValue.flatten()).toEqual(5)
+    })
+  })
+
   describe('.bind', () => {
     it('executes a function that receives the Maybe monad value', () => {
       const value = 5
 
-      const operatedMonadicValue = Maybe.of(value).bind(v => v ** 2)
+      const maybeValue = Maybe.of(value).bind(v => v ** 2)
 
-      expect(operatedMonadicValue.flatten()).toEqual(25)
+      expect(maybeValue.flatten()).toEqual(25)
     })
 
     it('allows functions to be composed to operate on the monadic value', () => {
       const value = 5
 
-      const operatedMonadicValue = Maybe.of(value)
+      const maybeValue = Maybe.of(value)
         .bind(v => v - 3)
         .bind(v => v ** 2)
 
-      expect(operatedMonadicValue.flatten()).toEqual(4)
+      expect(maybeValue.flatten()).toEqual(4)
     })
 
     it('returns a Maybe with nothing when one of the functions in the composition returns an invalid value', () => {
       const value = 5
 
-      const operatedMonadicValue = Maybe.of(value)
+      const maybeValue = Maybe.of(value)
         .bind(v => v - 3)
         .bind(() => null)
         .bind(v => v ** 2)
 
-      expect(operatedMonadicValue.flatten()).toEqual(Maybe.Nothing)
+      expect(maybeValue.flatten()).toEqual(Maybe.Nothing)
     })
   })
 
-  describe('.lift', () => {})
+  describe('.orElse', () => {
+    it('returns the monad value if it is in a valid state', () => {
+      const value = 5
+
+      const maybeValue = Maybe.of(value)
+
+      expect(maybeValue.orElse('another value')).toEqual(5)
+    })
+
+    it('returns the value given to the function if the monad holds a value in an invalid state', () => {
+      const value = undefined
+
+      const maybeValue = Maybe.of(value)
+
+      expect(maybeValue.orElse('another value')).toEqual('another value')
+    })
+  })
+
+  describe('.lift', () => {
+    it('turns a function that operates with a value to a function that operates with a monadic value', () => {
+      const f = (v) => v ** 2
+
+      const liftedF = Maybe.lift(f)
+
+      expect(f(undefined)).toEqual(NaN)
+      expect(liftedF(undefined).flatten()).toEqual(Maybe.Nothing)
+    })
+  })
 })
